fa2c9df84f473694fda54aec23fd64c8f47eec8d
jhemi-cristina <jhemilycristina@gmail.com>
2022-02-25 14:33:59 -0300

    how they work as interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
+// Escrevendo a função soma com typescript
 function soma(a: number, b: number) {
   return a + b;
 }
+
+// Como funcionam as interfaces
+// types
+// interfaces
+
+interface IAnimal {
+  nome: string;
+  tipo: "terrestre" | "aquático";
+  executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+  nome: "Elefante",
+  tipo: "terrestre",
+  executarRugido: (alturaEmDecibeis) => `${alturaEmDecibeis}dB`,
+};
+
+// const felino: IFelino = {
+//   nome: "Leão",
+//   tipo: "terrestre",
+//   visaoNoturna: true,
+// };
